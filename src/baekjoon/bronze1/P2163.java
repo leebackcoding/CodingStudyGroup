15e98883dab86bcd9f66d659787005134ea6b93e
leebackcoding <lcy2825@gmail.com>
2024-08-07 21:40:28 +0900

    Baekjoon_bronze1

---
+++ src/baekjoon/bronze1/P2163.java
@@ -0,0 +1,19 @@
+package baekjoon.bronze1;
+
+import java.io.*;
+import java.util.*;
+
+public class P2163 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int answer = (n - 1) + n * (m - 1);
+
+        System.out.println(answer);
+
+    }
+}
