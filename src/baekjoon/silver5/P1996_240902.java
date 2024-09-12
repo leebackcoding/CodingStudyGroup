55398215f19d8cb2969069daaff57fdcca6c4d0d
leebackcoding <lcy2825@gmail.com>
2024-09-12 23:33:40 +0900

    baekjoon_silver5_P1996

--- src/baekjoon/silver5/P1996_240902.java
+++ src/baekjoon/silver5/P1996_240902.java
@@ -6,49 +6,6 @@ import java.io.InputStreamReader;
 
 public class P1996_240902 {
 
-//    public static void main(String[] args) throws IOException {
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//
-//        int N = Integer.parseInt(br.readLine());
-//        String[][] map = new String[N][N];
-//        int[][] ziro = new int[N][N];
-//
-//        int[][] adj = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
-//
-//        for (int i = 0; i < N; i++) {
-//            map[i] = br.readLine().split("");
-//        }
-//
-//        StringBuilder sb = new StringBuilder();
-//
-//        for (int i = 0; i < N; i++) {
-//            for (int j = 0; j < N; j++) {
-//                if (!map[i][j].equals(".")) {
-//                    sb.append("*");
-//                    continue;
-//                } else {
-//                    for (int[] a : adj) {
-//                        int x = a[0] + j;
-//                        int y = a[1] + i;
-//                        if(isIn(x, y, N) && !map[y][x].equals(".")) {
-//                            ziro[i][j] += Integer.parseInt(map[y][x]);
-//                        }
-//                    }
-//                }
-//
-//                if (ziro[i][j] >= 10) {
-//                    sb.append("M");
-//                } else {
-//                    sb.append(ziro[i][j]);
-//                }
-//
-//            }
-//            sb.append("\n");
-//        }
-//
-//        System.out.println(sb);
-//    }
-
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -56,7 +13,6 @@ public class P1996_240902 {
         int N = Integer.parseInt(br.readLine());
         String[][] map = new String[N][N];
         int[][] ziro = new int[N][N];
-        String[][] answers = new String[N][N];
 
         int[][] adj = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
 
@@ -66,34 +22,27 @@ public class P1996_240902 {
 
         StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (!map[i][j].equals(".")) {
-                    int num = Integer.parseInt(map[i][j]);
-                    for (int[] a : adj) {
+        for (int i = 0; i < N; i++) { // 행번호
+            for (int j = 0; j < N; j++) { // 열번호
+                if (!map[i][j].equals(".")) { //지뢰라면
+                    sb.append("*"); // 별대입
+                    continue; // 다음꺼 검사
+                } else { // 지뢰가 아닐 때
+                    for (int[] a : adj) { // 인접 검사
                         int x = a[0] + j;
                         int y = a[1] + i;
-                        if(isIn(x, y, N) && map[y][x].equals(".")) {
-                            ziro[y][x] += num;
+                        if(isIn(x, y, N) && !map[y][x].equals(".")) {
+                            ziro[i][j] += Integer.parseInt(map[y][x]);
                         }
                     }
-
-                    answers[i][j] = "*";
                 }
 
-            }
-        }
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (answers[i][j] == null) {
-                    if (ziro[i][j] >= 10) {
-                        answers[i][j] = "M";
-                    } else {
-                        answers[i][j] = String.valueOf(ziro[i][j]);
-                    }
+                if (ziro[i][j] >= 10) {
+                    sb.append("M");
+                } else {
+                    sb.append(ziro[i][j]);
                 }
-                sb.append(answers[i][j]);
+
             }
             sb.append("\n");
         }
@@ -101,6 +50,59 @@ public class P1996_240902 {
         System.out.println(sb);
     }
 
+
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//        int N = Integer.parseInt(br.readLine());
+//        String[][] map = new String[N][N]; // 처음 입력 배열
+//        int[][] ziro = new int[N][N]; // 근처 지뢰를 넣어주기
+//        String[][] answers = new String[N][N]; // 정답 배열
+//
+//        // 검사해야할 인접 좌표
+//        int[][] adj = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
+//
+//        for (int i = 0; i < N; i++) {
+//            map[i] = br.readLine().split("");
+//        }
+//
+//        StringBuilder sb = new StringBuilder();
+//
+//        for (int i = 0; i < N; i++) { // 행 번호
+//            for (int j = 0; j < N; j++) { // 열 번호
+//                if (!map[i][j].equals(".")) { // 지뢰라면
+//                    int num = Integer.parseInt(map[i][j]); // 지뢰가 몇개인지 int로 변환
+//                    for (int[] a : adj) { //{0, 1}, {0, -1}
+//                        int x = a[0] + j;
+//                        int y = a[1] + i;
+//                        if(isIn(x, y, N) && map[y][x].equals(".")) {
+//                            ziro[y][x] += num;
+//                        }
+//                    }
+//
+//                    answers[i][j] = "*";
+//                }
+//            }
+//        }
+//
+//        for (int i = 0; i < N; i++) { // 행번호
+//            for (int j = 0; j < N; j++) { // 열번호
+//                if (answers[i][j] == null) { // 지뢰가 없는 자리
+//                    if (ziro[i][j] >= 10) { // 지뢰가 10개 이상이라면
+//                        answers[i][j] = "M"; // M
+//                    } else { // 지뢰가 10개 미만
+//                        answers[i][j] = String.valueOf(ziro[i][j]);
+//                    }
+//                }
+//                sb.append(answers[i][j]);
+//            }
+//            sb.append("\n"); // 줄바꿈, 개행
+//        }
+//
+//        System.out.println(sb);
+//    }
+
+    // 배열 안에 존재하는지 판단하는 메서드
     private static boolean isIn(int x, int y, int N) {
         return x >= 0 && x < N && y >= 0 && y < N;
     }
